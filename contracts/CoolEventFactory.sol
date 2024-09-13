24f7ee3c7216e9e7168bfa8b16c8b273bcca9a88
obah <olusojiobah@gmail.com>
2024-09-13 17:08:57 +0100

    chore: change factory to create2

--- contracts/CoolEventFactory.sol
+++ contracts/CoolEventFactory.sol
@@ -1,23 +1,51 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-import "./CoolEvent.sol";
-
 contract CoolEventFactory {
-    CoolEvent[] coolEvents;
+    event ContractDeployed(address indexed contractAddress);
 
-    function createCoolEvent(
-        uint40 _max_guests,
-        address _nft
-    ) external returns (CoolEvent coolEvent_, uint256 length_) {
-        coolEvent_ = new CoolEvent(_max_guests, _nft);
+    address[] public contractAddresses;
 
-        coolEvents.push(coolEvent_);
+    function createCoolEvent(bytes memory _bytecode,
+        uint256 _salt) external returns (address coolEventAddr_, uint256 length_) {
+            address contractAddress;
 
-        length_ = coolEvents.length;
+            assembly {
+            contractAddress := create2(
+                0,
+                add(_bytecode, 0x20),
+                mload(_bytecode),
+                _salt
+            )
+            if iszero(extcodesize(contractAddress)) {
+                revert(0, 0)
+            }
+
+            contractAddresses.push(contractAddress);
+            
+        length_ = contractAddresses.length;
+
+        emit ContractDeployed(contractAddress);
+        }
+        }
+        
+
+    function getContracts() external view returns (address[] memory) {
+        return contractAddresses;
     }
 
-    function getCoolEvents() external view returns (CoolEvent[] memory) {
-        return coolEvents;
-    }
+    // function createCoolEvent(
+    //     uint40 _max_guests,
+    //     address _nft
+    // ) external returns (CoolEvent coolEvent_, uint256 length_) {
+    //     coolEvent_ = new CoolEvent(_max_guests, _nft);
+
+    //     coolEvents.push(coolEvent_);
+
+    //     length_ = coolEvents.length;
+    // }
+
+    // function getCoolEvents() external view returns (CoolEvent[] memory) {
+    //     return coolEvents;
+    // }
 }
--- utils/uploadToPinata.ts
+++ utils/uploadToPinata.ts
@@ -2,10 +2,10 @@ import { PinataSDK } from "pinata";
 import fs from "fs";
 // const { Blob } = require("buffer");
 
-export async function uploadToPinata() {
+async function uploadToPinata() {
   const pinata = new PinataSDK({
     pinataJwt: process.env.PINATA_API_JWT!,
-    pinataGateway: "example-gateway.mypinata.cloud",
+    pinataGateway: "violet-nice-felidae-140.mypinata.cloud",
   });
 
   const imageLocation = "./assets/nft-image.svg";
@@ -15,23 +15,30 @@ export async function uploadToPinata() {
   const upload = await pinata.upload.file(file);
   const imageCid = upload.cid;
 
-  const metadata = {
-    title: "Asset Metadata",
-    type: "object",
-    properties: {
-      name: {
-        type: "string",
-        description: "Identifies the asset to which this NFT represents",
-      },
-      description: {
-        type: "string",
-        description: "Describes the asset to which this NFT represents",
-      },
-      image: {
-        type: "string",
-        description:
-          "A URI pointing to a resource with mime type image/* representing the asset to which this NFT represents. Consider making any images at a width between 320 and 1080 pixels and aspect ratio between 1.91:1 and 4:5 inclusive.",
-      },
-    },
-  };
+  // const metadata = {
+  //   title: "Asset Metadata",
+  //   type: "object",
+  //   properties: {
+  //     name: {
+  //       type: "string",
+  //       description: "Identifies the asset to which this NFT represents",
+  //     },
+  //     description: {
+  //       type: "string",
+  //       description: "Describes the asset to which this NFT represents",
+  //     },
+  //     image: {
+  //       type: "string",
+  //       description:
+  //         "A URI pointing to a resource with mime type image/* representing the asset to which this NFT represents. Consider making any images at a width between 320 and 1080 pixels and aspect ratio between 1.91:1 and 4:5 inclusive.",
+  //     },
+  //   },
+  // };
+
+  console.log(imageCid);
 }
+
+uploadToPinata().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
